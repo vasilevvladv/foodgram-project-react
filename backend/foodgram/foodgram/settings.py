2f644c1cbadb995fef4c831cf8c5bef926e82bf9
vasilevva <92749127+vasilevva@users.noreply.github.com>
2022-07-22 13:37:24 +0300

    Update settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -146,6 +146,11 @@ DJOSER = {
 
 EMPTY_VALUE = '-empty-'
 
-CSRF_TRUSTED_ORIGINS = os.getenv(
-    'CSRF_TRUSTED_ORIGINS', default='http://127.0.0.1'
-).split(',')
+CSRF_TRUSTED_ORIGINS = [
+    "http://*localhost",
+    "https://*localhost",
+    "http://*127.0.0.1",
+    "https://*127.0.0.1",
+    "http://*62.84.121.99",
+    "https://*62.84.121.99"
+]
